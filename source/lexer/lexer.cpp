2c2d581cf81bcd537b6e5d5c451a8e9bad99253c
PGHales <prestonghales@gmail.com>
2023-11-10 18:40:28 -0700

    Fix build errors

--- source/lexer/lexer.cpp
+++ source/lexer/lexer.cpp
@@ -6,4 +6,87 @@
 
 #include "lexer.hpp"
 
-namespace Lexer {}  // namespace Lexer
+namespace Lexer {
+
+    std::string tokenTypeToString(const TokenType& type) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    std::string readFile(const std::string& filePath) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    void tokenize(const TokenList& tokens, const std::string& filePath) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    FilePosition skipWhitespace(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexOp(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexTokenLiteral(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexFloat(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexInt(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexString(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexVariable(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    Token lexCommandVariable(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+
+    Token lexCommandString(const std::string& file, FilePosition& position) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    bool isLetter(const char& character) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    bool isDigit(const char& character) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    bool isWhitespace(const char& character) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+    bool isIllegalCharacter(const char& character) {
+        // TODO: Implement
+        throw new std::exception();
+    }
+
+}  // namespace Lexer
