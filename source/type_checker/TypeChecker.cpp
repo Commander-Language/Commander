242ba6f6262ad3bf90b3812727a63c8a3431c93f
JadenCG <jadengill887@gmail.com>
2023-11-20 10:31:04 -0700

    Finished initial implementation of TypeChecker features

--- source/type_checker/TypeChecker.cpp
+++ source/type_checker/TypeChecker.cpp
@@ -3,3 +3,58 @@
  */
 
 #include "TypeChecker.hpp"
+
+//Constructor
+TypeChecker::TypeChecker() {
+
+}
+
+//Destructor
+TypeChecker::~TypeChecker() {
+
+}
+
+//Copy-Constructor
+TypeChecker::TypeChecker(TypeChecker *otherTypeChecker) {
+    std::map<std::string, std::string> data(otherTypeChecker->assignedTypes);
+    assignedTypes = data;
+}
+
+/**
+ * setOrUpdateType() will attempt to assign a new variable to the current TypeChecker or update the current variable
+ * @param variableID - The desired variableID to update or add
+ * @param type - A string Type to associate the variable with
+ */
+void TypeChecker::setOrUpdateType(std::string variableID, std::string type) {
+    assignedTypes[variableID] = type;
+}
+
+/**
+ * getType() will attempt to return the variable type if present
+ * @param variableID - The desired variableID to obtain
+ * @return - A string type if the variable exists, otherwise nullptr is returned
+ */
+std::string TypeChecker::getType(std::string variableID) {
+    if(assignedTypes.find(variableID) == assignedTypes.end()) {
+        return nullptr;
+    }
+    return assignedTypes[variableID];
+}
+
+/**
+ * verifyType() will return a boolean if the variableID type is compatable with the expected type. This occurs in the following situations:
+ * - the expected type and variable type are equal
+ * - the expected type and variable type can be converted to one another (e.g. Int to Float)
+ * @param variableID - The variable one wishes to verify
+ * @param expected - The expected outcome for a variable
+ * @return - TRUE if a compatible type exists, otherwise FALSE is returned
+ */
+bool TypeChecker::verifyType(std::string variableID, std::string expected) {
+    if(assignedTypes.find(variableID) == assignedTypes.end()) {
+        return false; //return false if the variable doesnt exist
+    }
+    else if(assignedTypes[variableID] == expected) {
+        return true;
+    }
+    return true; //TODO: determine how similar types should be handled
+}
--- source/type_checker/TypeChecker.hpp
+++ source/type_checker/TypeChecker.hpp
@@ -6,6 +6,7 @@
 #ifndef CPP_UTILITIES_TYPECHECKER_HPP
 #define CPP_UTILITIES_TYPECHECKER_HPP
 #include <string>
+#include <map>
 
 
 class TypeChecker {
@@ -15,7 +16,11 @@ public:
     TypeChecker(TypeChecker *otherTypeChecker); //Copy-Constructor
 
     std::string getType(std::string variableID); //Returns the type of a variable
+    void setOrUpdateType(std::string variableID, std::string type); //Sets or updates the type of a variable
     bool verifyType(std::string variableID, std::string expected); //returns TRUE if the expected is equal to the actual type or can be used as the expected type
+
+private:
+    std::map<std::string, std::string> assignedTypes{}; //A dictionary which stores a string id and returns a string type
 
 };
 
