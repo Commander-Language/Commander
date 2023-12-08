5893ca7f7f82e410d0ec96d0201f20f35867641c
Cayden Lund <cayden.lund@utah.edu>
2023-12-07 20:28:48 -0700

    Implement the `ParseTable` class.

--- source/parser/parse_table.hpp
+++ source/parser/parse_table.hpp
@@ -2,12 +2,13 @@
  * @file source/parser/parse_table.hpp
  * @brief Defines the `ParseTable` class.
  * @details Provides transitions from parser state to state.
- *
  */
 
 #ifndef PARSE_TABLE_HPP
 #define PARSE_TABLE_HPP
 
+#include "ast_node.hpp"
+#include "grammar.hpp"
 #include "parser_action.hpp"
 
 #include "source/lexer/lexer.hpp"
@@ -20,20 +21,35 @@ namespace Parser {
 
     /**
      * @brief The table of state transitions that the parser follows.
-     *
      */
     class ParseTable {
     public:
         /**
+         * @brief Shorthand for the `TokenType` enumeration from the lexer.
+         */
+        using TokenType = lexer::tokenType;
+
+        /**
          * @brief Each state has a unique integer ID.
-         *
          */
         using StateNum = unsigned int;
 
         /**
+         * @brief Represents an entry in a grammar rule.
+         * @details Either a token type or an AST node type.
+         *     Every rule is made up of these.
+         */
+        using GrammarEntry = Grammar::GrammarEntry;
+
+        /**
+         * @brief Represents a single grammar rule.
+         * @details Made up of the resulting AST node type and a list of component `GrammarEntry`s.
+         */
+        using GrammarRule = Grammar::GrammarRule;
+
+        /**
          * @brief Class constructor.
          * @details Does a lot of initialization work under the hood. Avoid calling more than once.
-         *
          */
         ParseTable();
 
@@ -44,7 +60,7 @@ namespace Parser {
          * @param tokenType The type of the next unparsed token.
          * @return The next action for the parser to take.
          */
-        ParserAction getNextAction(StateNum stateNum, lexer::tokenType tokenType);
+        ParserAction getNextAction(StateNum stateNum, TokenType tokenType);
 
         /**
          * @brief Given the top AST node on the stack, reports the next state to use.
@@ -57,139 +73,34 @@ namespace Parser {
 
     private:
         /**
-         * @brief A method that makes a new AST node smart pointer.
-         * @details Uses a list of `ProductionItem`s.
-         *
-         */
-        using NodeConstructor = ParserAction::NodeConstructor;
-
-        /**
-         * @brief Shorthand for the `TokenType` enumeration from the lexer.
-         *
-         */
-        using TokenType = lexer::tokenType;
-
-        /**
-         * @brief A component of a grammar rule.
-         * @details Either a token type or an AST node type.
-         *
-         */
-        struct GrammarEntry {
-            /**
-             * @brief The token type of this `GrammarEntry`.
-             * @details Void if `nodeType` is set.
-             *
-             */
-            std::optional<TokenType> tokenType;
-
-            /**
-             * @brief The AST node type of this `GrammarEntry`.
-             * @details Void if `tokenType` is set.
-             *
-             */
-            std::optional<ASTNodeType> nodeType;
-
-            /**
-             * @brief Class constructor for a token type.
-             *
-             * @param tokenType The token type of this `GrammarEntry`.
-             */
-            GrammarEntry(TokenType tokenType);
-
-            /**
-             * @brief Class constructor for an AST node type.
-             *
-             * @param tokenType The AST node type of this `GrammarEntry`.
-             */
-            GrammarEntry(ASTNodeType nodeType);
-
-            /**
-             * @brief Equality operator.
-             *
-             * @param other The other `GrammarEntry` against which to compare.
-             * @return True if the two grammar entries are equal; false otherwise.
-             */
-            bool operator==(const GrammarEntry& other) const;
-
-            /**
-             * @brief Inequality operator.
-             *
-             * @param other The other `GrammarEntry` against which to compare.
-             * @return False if the two grammar entries are equal; true otherwise.
-             */
-            bool operator!=(const GrammarEntry& other) const;
-        };
-
-        /**
-         * @brief Hashing functor for `GrammarEntry` objects.
-         *
-         */
-        struct HashGrammarEntry {
-            /**
-             * @brief Hashes the given `GrammarEntry`.
-             *
-             * @param entry The grammar entry to hash.
-             * @return The hash value of the given grammar entry.
-             */
-            size_t operator()(const GrammarEntry& entry) const;
-        };
-
-        /**
-         * @brief Defines a grammar rule for constructing an AST node.
-         *
-         */
-        struct GrammarRule {
-            /**
-             * @brief The resulting AST node type.
-             *
-             */
-            ASTNodeType result;
-
-            /**
-             * @brief The parts that make up the grammar rule.
-             *
-             */
-            std::vector<GrammarEntry> components;
-
-            /**
-             * @brief Equality operator.
-             *
-             * @param other The other `GrammarRule` against which to compare.
-             * @return True if the two grammar rules are equal; false otherwise.
-             */
-            bool operator==(const GrammarRule& other) const;
-
-            /**
-             * @brief Inequality operator.
-             *
-             * @param other The other `GrammarRule` against which to compare.
-             * @return False if the two grammar rules are equal; true otherwise.
-             */
-            bool operator!=(const GrammarRule& other) const;
-        };
-
-        /**
-         * @brief Hashing functor for `GrammarRule` objects.
-         *
-         */
-        struct HashGrammarRule {
-            /**
-             * @brief Hashes the given `GrammarRule`.
-             *
-             * @param rule The grammar rule to hash.
-             * @return The hash value of the given grammar rule.
-             */
-            size_t operator()(const GrammarRule& rule) const;
-        };
-
-        /**
          * @brief A lookahead kernel for building the parse table.
          * @details Consists of a grammar rule,
          *     how far into the grammar rule we are,
          *     and a lookahead token type.
-         *
          */
         struct Kernel {
+            /**
+             * @brief Hashing functor for `Kernel` objects.
+             */
+            struct Hash {
+                /**
+                 * @brief Hashes the given `Kernel`.
+                 *
+                 * @param kernel The kernel to hash.
+                 * @return The hash value of the given kernel.
+                 */
+                size_t operator()(const Kernel& kernel) const;
+            };
+
+            /**
+             * @brief Class constructor.
+             *
+             * @param rule The `GrammarRule` to which this kernel refers.
+             * @param index How far we are into the `GrammarRule`.
+             * @param lookahead The next lookahead token type.
+             */
+            Kernel(const GrammarRule& rule, size_t index, TokenType lookahead);
+
             /**
              * @brief Equality operator.
              *
@@ -208,48 +119,51 @@ namespace Parser {
 
             /**
              * @brief The `GrammarRule` to which this kernel refers.
-             *
              */
-            const GrammarRule& rule;
+            std::reference_wrapper<const GrammarRule> rule;
 
             /**
              * @brief The index into the grammar rule.
-             *
              */
-            const size_t index;
+            size_t index;
 
             /**
              * @brief The lookahead token.
-             *
              */
-            const TokenType lookahead;
-        };
-
-        /**
-         * @brief Hashing functor for `Kernel` objects.
-         *
-         */
-        struct HashKernel {
-            /**
-             * @brief Hashes the given `Kernel`.
-             *
-             * @param kernel The kernel to hash.
-             * @return The hash value of the given kernel.
-             */
-            size_t operator()(const Kernel& kernel) const;
+            TokenType lookahead;
         };
 
         /**
          * @brief Represents a state in the parsing automaton.
-         *
          */
         struct State {
-            std::vector<Kernel> closure;
+            /**
+             * @brief Class constructor.
+             */
+            State();
+
+            /**
+             * @brief Class constructor.
+             *
+             * @param kernels All relevant kernels corresponding to this state.
+             * @param transitions All transitions from this state.
+             */
+            State(const std::vector<Kernel>& kernels,
+                  const std::unordered_map<GrammarEntry, StateNum, Grammar::GrammarEntry::Hash>& transitions = {});
+
+            /**
+             * @brief All relevant kernels corresponding to this state.
+             */
+            std::vector<Kernel> kernels;
+
+            /**
+             * @brief All transitions from this state.
+             */
+            std::unordered_map<GrammarEntry, StateNum, Grammar::GrammarEntry::Hash> transitions;
         };
 
         /**
          * @brief Represents the closure of all reachable kernels from any single kernel.
-         *
          */
         class Closure {
         public:
@@ -272,35 +186,22 @@ namespace Parser {
         private:
             /**
              * @brief A mapping from one kernel to the set of all reachable kernels from it.
-             *
              */
-            std::unordered_map<Kernel, std::vector<Kernel>, HashKernel> _closure;
+            std::unordered_map<Kernel, std::vector<Kernel>, Kernel::Hash> _closure;
 
             /**
              * @brief A reference to the grammar definition.
-             *
              */
             const std::vector<GrammarRule>& _grammar;
         };
 
         /**
-         * @brief Combines two hashed values in a deterministic way.
-         *
-         * @param hash1 The first hashed value.
-         * @param hash2 The second hashed value.
-         * @return A unique combination of the two hashed values.
-         */
-        static size_t _combineHashes(size_t hash1, size_t hash2);
-
-        /**
          * @brief A mapping of a state number and a token type to a parser action.
-         *
          */
         std::unordered_map<StateNum, std::unordered_map<TokenType, ParserAction>> _nextAction;
 
         /**
          * @brief A mapping of a state number an an AST node type to the parser's next state.
-         *
          */
         std::unordered_map<StateNum, std::unordered_map<ASTNodeType, StateNum>> _nextState;
     };
