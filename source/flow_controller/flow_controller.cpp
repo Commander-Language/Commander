af3ed2de6fefd3c648378c3ead24117b48f74cf8
Eduardo Valdivia <eddval0808@gmail.com>
2024-01-14 23:37:29 -0700

    Define some methods and some work

--- source/flow_controller/flow_controller.cpp
+++ source/flow_controller/flow_controller.cpp
@@ -2,4 +2,42 @@
 
 namespace FlowController {
     FlowController::FlowController(Parser::ASTNodeList& nodes) : _nodes(std::move(nodes)) {}
+    void FlowController::runCommand() {}
+    void FlowController::setVariable(std::string name, std::any value) {
+        _symbolTable.addOrUpdateVariable(std::move(name), 0);
+    }
+    void FlowController::runtime() {
+        for (auto& node : _nodes) {
+            switch (node->nodeType()) {
+                case Parser::BINDING: {
+
+                }
+                case Parser::BINDINGS: {
+                }
+                case Parser::CMD: {
+                }
+                case Parser::EXPR: {
+                }
+                case Parser::EXPRS: {
+                }
+                case Parser::PRGM: {
+                }
+                case Parser::STMT: {
+                }
+                case Parser::STMTS: {
+                }
+                case Parser::STRING: {
+                }
+                case Parser::TYPE: {
+                }
+                case Parser::VARIABLE: {
+                }
+            }
+        }
+    }
+
+    void FlowController::_doBindings(Parser::BindingNode node) {
+        node.
+    }
+
 }  // namespace FlowController
--- source/flow_controller/flow_controller.hpp
+++ source/flow_controller/flow_controller.hpp
@@ -1,15 +1,23 @@
 #ifndef FLOW_CONTROLLER_HPP
 #define FLOW_CONTROLLER_HPP
 
-#include "../parser/parser.hpp"
+#include "source/parser/parser.hpp"
+#include "source/symbol_table/symbol_table_organizer.hpp"
+
+#include <any>
 
 namespace FlowController {
     class FlowController {
     public:
         FlowController(Parser::ASTNodeList&);
+        void setVariable(std::string name, std::any value);
+        void runCommand();
+        void runtime();
 
     private:
+        void _doBindings(Parser::BindingNode);
         Parser::ASTNodeList _nodes;
+        SymbolTableOrganizer _symbolTable;
     };
 }  // namespace FlowController
 #endif
