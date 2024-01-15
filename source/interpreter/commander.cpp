dfbfa53a535771c332d7ccbe3fbf77237a6f63f8
pghales <u1171135@utah.edu>
2024-01-15 11:48:41 -0700

    Add a couple tests

--- source/interpreter/commander.cpp
+++ source/interpreter/commander.cpp
@@ -3,20 +3,45 @@
 #include "../util/commander_exception.hpp"
 #include <fstream>
 #include <iostream>
-#include <map>
-#include <variant>
+
+void interpretFile(std::string& fileName, std::string& option, Parser::Parser& parser) {
+    lexer::TokenList tokens;
+    lexer::tokenize(tokens, fileName);
+    if (option == "-l") {
+        for (const lexer::TokenPtr& token : tokens) std::cout << token->toString() << '\n';
+        return;
+    }
+    Parser::ASTNodeList nodes = parser.parse(tokens);
+    if (option == "-p") {
+        for (const Parser::ASTNodePtr& node : nodes) std::cout << node->sExpression() << '\n';
+        return;
+    }
+    if (option == "-t") {
+        // TODO: type checking
+        return;
+    }
+}
 
 int main(int argc, char** argv) {
     try {
-        std::string arg = "";
-        if (argc > 1) { arg = std::string(argv[1]); }
         clock_t start = clock();
         Parser::Parser parser;
         clock_t end = clock();
         std::cout << "Parse Table Initialization Time: " << ((double)(end - start) / CLOCKS_PER_SEC) << " seconds"
-                  << std::endl;
-        std::cout << "Commander Language Prototype" << std::endl;
-        std::cout << "Basic REPL for Commander scripting language." << std::endl;
+                  << '\n';
+        std::cout << "Commander Language Prototype" << '\n';
+        std::cout << "Basic REPL for Commander scripting language." << '\n';
+        std::string arg;
+        if (argc > 1) { arg = std::string(argv[1]); }
+        if (arg == "-f") {
+            if (argc == 2) {
+                throw util::CommanderException("No file name provided.");
+            }
+            std::string file = std::string(argv[2]);
+            std::string option = "-l";
+            interpretFile(file, option, parser);
+            return 0;
+        }
         while (true) {
             try {
                 std::cout << ">> ";
@@ -25,7 +50,7 @@ int main(int argc, char** argv) {
                 std::getline(std::cin, source);
 
                 if (source == "clear") {
-                    std::cout << "\033[2J\033[H" << std::endl;
+                    std::cout << "\033[2J\033[H" << '\n';
                     continue;
                 }
 
@@ -37,51 +62,14 @@ int main(int argc, char** argv) {
                 tmp << source;
                 tmp.close();  // close file here to save changes
 
-                lexer::TokenList tokens;
-                lexer::tokenize(tokens, tmpFileName);
+                interpretFile(tmpFileName, arg, parser);
+
                 std::remove(tmpFileName.c_str());
-                if (arg == "-l") {
-                    for (const lexer::TokenPtr& token : tokens) std::cout << token->toString() << std::endl;
-                    continue;
-                }
-                Parser::ASTNodeList nodes = parser.parse(tokens);
-                if (arg == "-p") {
-                    for (const Parser::ASTNodePtr& node : nodes) std::cout << node->sExpression() << std::endl;
-                    continue;
-                }
-                if (arg == "-t") {
-                    // TODO: type checking
-                    continue;
-                }
-                // TODO: Run script
-                std::map<std::string, std::variant<long, double, bool>> variables;
-                for (const Parser::ASTNodePtr& node : nodes) {
-                    if (node->nodeType() == Parser::ASTNodeType::STMTS) {
-                        switch (node->nodeType()) {
-                            // Basic commands
-                            case Parser::ASTNodeType::CMD:
-                                // TODO
-                                break;
-                            // Variables
-                            case Parser::ASTNodeType::VARIABLE:
-                                // TODO
-                                break;
-                            // Expressions (math, boolean, variables, strings and string interpolation)
-                            case Parser::ASTNodeType::EXPRS:
-                                // TODO
-                                break;
-                            // User I/O (need to handle read, write, scan, print)
-                            case Parser::ASTNodeType::STMT:
-                                // TODO
-                                break;
-                        }
-                    }
-                }
-            } catch (const util::CommanderException& err) { std::cout << err.what() << std::endl; }
+            } catch (const util::CommanderException& err) { std::cout << err.what() << '\n'; }
         }
         return 0;
     } catch (const util::CommanderException& e) {
-        std::cout << e.what() << std::endl;
+        std::cout << e.what() << '\n';
         return 1;
     }
 }
--- tests/lexer_tests.hpp
+++ tests/lexer_tests.hpp
@@ -69,7 +69,8 @@ INSTANTIATE_TEST_SUITE_P(
                 std::make_tuple("077.cmdr", "077.cmdr.expected"), std::make_tuple("078.cmdr", "078.cmdr.expected"),
                 std::make_tuple("079.cmdr", "079.cmdr.expected"), std::make_tuple("080.cmdr", "080.cmdr.expected"),
                 std::make_tuple("081.cmdr", "081.cmdr.expected"), std::make_tuple("082.cmdr", "082.cmdr.expected"),
-                std::make_tuple("083.cmdr", "083.cmdr.expected"), std::make_tuple("084.cmdr", "084.cmdr.expected")));
+                std::make_tuple("083.cmdr", "083.cmdr.expected"), std::make_tuple("084.cmdr", "084.cmdr.expected"),
+                std::make_tuple("085.cmdr", "085.cmdr.expected"), std::make_tuple("086.cmdr", "086.cmdr.expected")));
 
 /**
  * Creates test suite, with list of all the files that need to be lexed that are expected to fail (as a result of a
